b2ec8a5c44f48b3e8e060c6543c6edfb1245c393
htmlza2555 <P.nopanan@kkumail.com>
2023-10-13 12:38:07 +0700

    Register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,75 @@
+import { NavLink } from 'react-router-dom'
 import classes from './Register.module.css'
 
 const Register = () => {
   return (
-    <div className={classes.container}>
-      <div></div>
-    </div>
+    <form className={classes.container}>
+      <h1 style={{ color: '#FF731D', fontSize: '36px' }}>Register</h1>
+      <div className={classes.regis}>
+        <div className={classes.inputGroup}>
+          <div>Username</div>
+          <input
+            style={{
+              width: '100%',
+              height: '40px',
+              borderRadius: '10px',
+              fontSize: '20px',
+              color: '#7f8588',
+              borderColor: '#7f8588',
+            }}
+            required
+          />
+        </div>
+        <div className={classes.inputGroup}>
+          <div>Your Name</div>
+          <input
+            style={{
+              width: '100%',
+              height: '40px',
+              borderRadius: '10px',
+              fontSize: '20px',
+              color: '#7f8588',
+              borderColor: '#7f8588',
+            }}
+            required
+          />
+        </div>
+        <div className={classes.inputGroup}>
+          <div>Password</div>
+          <input
+            style={{
+              width: '100%',
+              height: '40px',
+              borderRadius: '10px',
+              fontSize: '20px',
+              color: '#7f8588',
+              borderColor: '#7f8588',
+            }}
+            required
+          />
+        </div>
+        <div className={classes.inputGroup}>
+          <div>Confirm Password</div>
+          <input
+            style={{
+              width: '100%',
+              height: '40px',
+              borderRadius: '10px',
+              fontSize: '20px',
+              color: '#7f8588',
+              borderColor: '#7f8588',
+            }}
+            required
+          />
+        </div>
+        <button className={classes.register_btn} type="submit" disabled>
+          Register
+        </button>
+        <NavLink to="/login" style={{ textDecoration: 'none', color: '#848c92', textAlign: 'center' }}>
+          {`Already have an account? Login`}
+        </NavLink>
+      </div>
+    </form>
   )
 }
 
